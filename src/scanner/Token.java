e8af82b5e68e54d105246b646a3e69e5bd477762
HeeChan Kang <kang@augsburg.edu>
2018-01-09 22:57:56 -0600

    Made modifications to Token.toString() to tab correctly with longer token type string length. Also tried testing for more things by making modifications to test code provided by professor.

--- src/scanner/Token.java
+++ src/scanner/Token.java
@@ -14,5 +14,16 @@ public class Token {
 	}
     
     @Override
-    public String toString() { return "Token: " + this.type + "\t| Lexeme: " + this.lexeme; }
+    public String toString() { 
+    	if (this.type == TokenType.LESSTHANOREQUALTO ||
+    			this.type == TokenType.GREATERTHANOREQUALTO ||
+    			this.type == TokenType.LSQBRACKET ||
+				this.type == TokenType.RSQBRACKET ||
+				this.type == TokenType.GREATERTHAN ||
+    			this.type == TokenType.LPARENTHESES ||
+    			this.type == TokenType.RPARENTHESES) {
+    		return "Token: " + this.type + "\t| Lexeme: " + this.lexeme;
+    	}
+    	return "Token: " + this.type + "\t\t| Lexeme: " + this.lexeme;
+    }
 }
