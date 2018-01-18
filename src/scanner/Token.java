3cf9859303e8bea73a499028ec585b5b794df3fb
HeeChan Kang <kang@augsburg.edu>
2018-01-18 14:55:24 -0600

    Removed hashCode() method in Token class as it was unnecessary.

--- src/scanner/Token.java
+++ src/scanner/Token.java
@@ -27,14 +27,6 @@ public class Token {
     	return "Token: " + this.type + "\t\t| Lexeme: " + this.lexeme;
     }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((lexeme == null) ? 0 : lexeme.hashCode());
-		result = prime * result + ((type == null) ? 0 : type.hashCode());
-		return result;
-	}
 
 	@Override
 	public boolean equals(Object obj) {
