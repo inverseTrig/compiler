f2953daef147847fdf67db0bff3cc967ace6bc62
HeeChan Kang <kang@augsburg.edu>
2018-01-25 14:32:56 -0600

    Created Parser package, Parser.java, and match() and error().

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,9 +1,12 @@
 package parser;
+import java.io.IOException;
+
 import scanner.*;
 
 public class Parser {
 	
-	
+	private Token lookAhead;
+	private Scanner scanner;
 	
 	
 	
@@ -12,5 +15,20 @@ public class Parser {
 		
 	}
 	
+	public void match(TokenType expected) {
+		if (this.lookAhead.getType() == expected) {
+			try {
+				this.lookAhead = scanner.nextToken();
+				if (this.lookAhead == null) {
+					this.lookAhead = new Token("EOF", null);
+				}
+			} catch (IOException e) {
+				error("IOException from Scanner");
+			}
+		}
+	}
 	
+	public void error (String message) {
+		System.out.println("Error " + message);
+	}
 }
