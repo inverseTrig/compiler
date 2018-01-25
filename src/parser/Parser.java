e4aadb71a35669cfb11314886fd1bb8484dbeee8
HeeChan Kang <kang@augsburg.edu>
2018-01-25 14:47:32 -0600

    Initialized empty constructors for all 24.

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -9,11 +9,109 @@ public class Parser {
 	private Scanner scanner;
 	
 	
-	
-	
 	public void program() {
+		match(TokenType.PROGRAM);
+		match(TokenType.ID);
+		match(TokenType.SEMICOLON);
+		declarations();
+		subprogram_declarations();
+		compound_statement();
+		match(TokenType.PERIOD);
+	}
+	
+	public void identifier_list() {
 		
 	}
+	
+	public void declarations() {
+		
+	}
+	
+	public void type() {
+		
+	}
+	
+	public void standard_type() {
+		
+	}
+	
+	public void subprogram_declarations() {
+		
+	}
+	
+	public void subprogram_declaration() {
+		
+	}
+	
+	public void subprogram_head() {
+		
+	}
+	
+	public void arguments() {
+		
+	}
+	
+	public void parameter_list() {
+		
+	}
+	
+	public void compound_statement() {
+		
+	}
+	
+	public void optional_statements() {
+		
+	}
+	
+	public void statement_list() {
+		
+	}
+	
+	public void statement() {
+		
+	}
+	
+	public void variable() {
+		
+	}
+	
+	public void procedure_statement() {
+		
+	}
+	
+	public void expression_list() {
+		
+	}
+	
+	public void expression() {
+		
+	}
+	
+	public void simple_expression() {
+		
+	}
+	
+	public void simple_part() {
+		
+	}
+	
+	public void term() {
+		
+	}
+	
+	public void term_part() {
+		
+	}
+	
+	public void factor() {
+		
+	}
+	
+	public void sign() {
+		
+	}
+	
+	
 	
 	public void match(TokenType expected) {
 		if (this.lookAhead.getType() == expected) {
