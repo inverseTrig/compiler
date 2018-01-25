2b3ddca28a58177fe7b8233e2e82d4f34d5ed7f0
HeeChan Kang <kang@augsburg.edu>
2018-01-25 15:44:58 -0600

    Added statement_list().

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -125,7 +125,14 @@ public class Parser {
 	}
 	
 	public void statement_list() {
-		
+		if (lookAhead.getType() == TokenType.ID || lookAhead.getType() == TokenType.BEGIN ||
+				lookAhead.getType() == TokenType.IF || lookAhead.getType() == TokenType.WHILE) {
+			statement();
+		}
+		if (lookAhead.getType() == TokenType.SEMICOLON) {
+			match(TokenType.SEMICOLON);
+			statement_list();
+		}
 	}
 	
 	public void statement() {
