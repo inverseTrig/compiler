bbea6d2e7c389413c1a06cee680d6e7058119a71
HeeChan Kang <kang@augsburg.edu>
2018-03-04 13:26:45 -0600

    Added additional test case.

--- src/parser/SyntaxTreeTest.java
+++ src/parser/SyntaxTreeTest.java
@@ -16,7 +16,7 @@ class SyntaxTreeTest {
 	 * Creating a ProgramNode manually and compare it to the one Parser creates.
 	 */
 	@Test
-	void testProgramNode() {
+	void testProgramNodeA() {
 		ProgramNode pNode = new ProgramNode("foo");
 		pNode.setVariables(new DeclarationsNode());
 		pNode.setFunctions(new SubProgramDeclarationsNode());
@@ -32,5 +32,25 @@ class SyntaxTreeTest {
 		assertEquals(expected, actual);
 	}
 	
-	
+	/**
+	 * Creating a ProgramNode manually and compare it to the one Parser creates.
+	 * Different than the first one, here we add additional variables, but of course, as it hasn't been implemented,
+	 * this test should pass as well.
+	 */
+	@Test
+	void testProgramNodeB() {
+		ProgramNode pNode = new ProgramNode("foo");
+		pNode.setVariables(new DeclarationsNode());
+		pNode.setFunctions(new SubProgramDeclarationsNode());
+		pNode.setMain(new CompoundStatementNode());
+		
+		String expected = pNode.indentedToString(0);
+		
+		String test = "program foo; var num, f : integer; begin end .";
+		Parser p = new Parser(test, false);
+		
+		String actual = p.program().indentedToString(0);
+		
+		assertEquals(expected, actual);
+	}
 }
